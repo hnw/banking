b6af10bf242c0fa608aefd2987590d79416d976b
Yoshio HANAWA <y@hnw.jp>
2019-09-23 20:28:30 +0900

    transferサブコマンドを改善、オプションを維持したままコマンド呼び直すようにした

--- banks/transfer.js
+++ banks/transfer.js
@@ -3,10 +3,17 @@ const { spawnSync } = require('child_process');
 const command = [ 'transfer <src> <dst> <amount>', '振込', '銀行振込', '送金' ];
 const description = '指定銀行に指定金額を振込';
 const handler = function (argv) {
-  const ret = spawnSync('node',
-                        [ argv.$0, argv.src, 'transfer', argv.dst, argv.amount ],
-                        { stdio: 'inherit' });
-  console.log(ret);
+  const command = process.argv[0];
+  const scriptPath = process.argv[1];
+  let args = process.argv.slice(2);
+  const subcommand = argv._[0]
+  const subcommandIndex = args.findIndex(el => el === subcommand);
+  const srcIndex = args.findIndex(el => el === argv.src);
+  // swap subcommand and src
+  args[subcommandIndex] = argv.src;
+  args[srcIndex] = subcommand;
+  args.unshift(scriptPath)
+  const ret = spawnSync(command, args, { stdio: 'inherit' });
 };
 const builder = function (yargs) {
   yargs.strict(false)
